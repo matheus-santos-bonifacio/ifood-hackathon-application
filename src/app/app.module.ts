baedc3580942f3f19fecfd079b61e065f05b3ebf
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2022-02-20 11:29:24 -0300

    fix (home): removing worthless framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { SwiperModule } from 'swiper/angular';
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SwiperModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
